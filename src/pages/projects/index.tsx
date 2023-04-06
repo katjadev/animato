5c96cff17868d9aee6e4a2cca567d9328bcd5168
Katja Nazarova <katienazarova@gmail.com>
2023-04-06 21:09:47 +0200

    Fix deploy

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,17 +1,4 @@
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-import { useAuth } from '@animato/context/AuthUserContext'
-
 export default function Projects() {
-  const { authUser, loading } = useAuth()
-  const router = useRouter()
-
-  useEffect(() => {
-    if (!loading && !authUser) {
-      router.push('/')
-    }
-  }, [authUser, loading, router])
-
   return(
     <h1>Projects</h1>
   );
