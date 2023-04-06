c83a313a628b008873215cee792242c3c6c19654
Katja Nazarova <katienazarova@gmail.com>
2023-04-06 21:08:12 +0200

    Fix deploy

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,6 @@
 import type { AppProps } from 'next/app'
-import { AuthUserProvider } from '@animato/context/AuthUserContext'
-
 import '@animato/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <AuthUserProvider><Component {...pageProps} /></AuthUserProvider>
+  return <Component {...pageProps} />
 }
